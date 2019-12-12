9b18883cacc75342d1bea06b109e5dc4a0048e44
serhii.mazur <serhii.mazur@beetroot.se>
2019-12-12 23:35:12 +0200

    upade script

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-function isIE() {
-  var ua = window.navigator.userAgent;
-  var msie = ua.indexOf('MSIE '); // IE 10 or older
-  var trident = ua.indexOf('Trident/'); //IE 11
+jQuery(document).ready(() => {
 
-  return (msie > 0 || trident > 0);
-}
+  function isIE() {
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf('MSIE '); // IE 10 or older
+    var trident = ua.indexOf('Trident/'); //IE 11
 
-if (isIE()) {
-  console.log('!!! SOS !!!');
-}
+    return (msie > 0 || trident > 0);
+  }
+
+  if (!isIE()) {
+    alert('Not IE browser');
+  }
+
+  $('.close-notice').click(function(){
+    $('.browser-notice').remove();
+  });
+
+  setTimeout(function(){
+    if (!isIE()) {
+      alert('Not IE browser');
+    };
+
+
+  }, 5000)
+
+})
