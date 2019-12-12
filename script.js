a823b7ac353a1961c8efa747eb3a661c548e387b
serhii.mazur <serhii.mazur@beetroot.se>
2019-12-12 20:10:23 +0200

    updates 12.12

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log(navigator.userAgent);
+function isIE() {
+  var ua = window.navigator.userAgent;
+  var msie = ua.indexOf('MSIE '); // IE 10 or older
+  var trident = ua.indexOf('Trident/'); //IE 11
+
+  return (msie > 0 || trident > 0);
+}
+
+if (isIE()) {
+  console.log('!!! SOS !!!');
+}
